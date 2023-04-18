4b9cdb2937a414c8768f3daf2f1889a5321f32b9
Jack Gabriel C. Acodili <112671361+acodili-j-g@users.noreply.github.com>
2023-04-18 08:10:46 +0000

    Replace with Setup Outputs

---
+++ html/src/io/github/jgacodili/molecules/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package io.github.jgacodili.molecules.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import io.github.jgacodili.molecules.Molecules;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Molecules();
+        }
+}
